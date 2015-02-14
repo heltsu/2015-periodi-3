cf629ff82bfb66a6314be92b7518135764a436d7
Heltsu <heli.niemimaa@helsinki.fi>
2015-02-14 14:18:33 +0200

    Eka testi

---
+++ TiraLabra/src/tiralabra/TiraLabra.java
@@ -0,0 +1,17 @@
+
+package tiralabra;
+
+/**
+ *
+ * @author Heli
+ */
+public class TiraLabra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Eka testi!");
+    }
+    
+}
